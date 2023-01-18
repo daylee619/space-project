75e46b1baebc0554554073c23e8afeb847912a09
daylee619 <110576332+daylee619@users.noreply.github.com>
2023-01-18 21:06:16 +0900

    중간저장

---
+++ pages/productlist/index.js
@@ -0,0 +1,9 @@
+import ProductList from "../../src/components/units/productList"
+
+export default function ProductListPage() {
+  return (
+    <>
+      <ProductList />
+    </>
+  )
+}
--- src/components/units/productList/index.js
+++ src/components/units/productList/index.js
@@ -1,6 +1,8 @@
 import axios from "axios"
+import { HeartOutlined } from "@ant-design/icons"
 import { useEffect, useState } from "react"
 import ProductItem from "../../../common/libraries/productItem/ProductItem"
+import styled from "@emotion/styled"
 
 export default function ProductList() {
   const [data, setData] = useState([])
@@ -11,34 +13,59 @@ export default function ProductList() {
     })
   }, [])
 
-  const temp = [
-    "(39) Ivory / SIZE : S(085)",
-    "(39) Ivory / SIZE : M(090)",
-    "(39) Ivory / SIZE : L(095)",
-  ]
-
-  // const a = data.map((el) => el.id)
-  // console.log(a)
   //   console.log(data)
 
+  const dataBox = data.map((el) => el.color.map((el) => el.size))
+  console.log(dataBox)
   return (
     <>
       <div>Women</div>
-
-      {data.map((el) => (
-        <div
-          key={el.id}
-          style={{ margin: "10px" }}
-        >
-          <ProductItem
-            imgWidth="320px"
-            imgHeight="400px"
-            menuWidth="200px"
-            imgUrl={el.thumbnail}
-            data={temp}
-          />
-        </div>
-      ))}
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          width: "985px",
+          flexWrap: "wrap",
+        }}
+      >
+        {data.map((el) => (
+          <div
+            key={el.id}
+            style={{ margin: "10px" }}
+          >
+            <ProductItem
+              imgWidth="300px"
+              imgHeight="400px"
+              menuWidth="150px"
+              bottom="400px"
+              imgUrl={el.thumbnail}
+              data={dataBox}
+            />
+            <div>
+              <ItemName>{el.name}</ItemName>
+              <HeartOutlined />
+              <ItemPrice>{el.price}</ItemPrice>
+              <div>
+                {el.color.map((el) => (
+                  <div key={el.id}>{el.colorId}</div>
+                ))}
+              </div>
+              <div>리뷰 0건</div>
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   )
 }
+
+export const ItemName = styled.div`
+  font-size: 16px;
+  color: #1a1a1a;
+  font-family: "Poppins", "Noto Sans KR", Verdana, Dotum, AppleGothic,
+    sans-serif;
+`
+export const ItemPrice = styled.div`
+  font-size: 22px;
+  font-weight: 500;
+`
