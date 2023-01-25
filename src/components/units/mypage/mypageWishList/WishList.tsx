71c5da7f867a989d9abeeea62b6349217866440a
JeongHyeonSeok <hyeonseok45@gmail.com>
2023-01-25 14:59:15 +0900

    chekcout save

--- src/components/units/mypage/mypageWishList/WishList.tsx
+++ src/components/units/mypage/mypageWishList/WishList.tsx
@@ -4,7 +4,36 @@ const WishList = () => {
     return (
         <S.Contain>
             <S.PageTitle>위시리스트</S.PageTitle>
-            <div></div>
+            <S.ItemTitleBox>
+                <S.ItemTitleCheckBox type='checkbox' />
+                <S.ItemTitleInfo>상품정보</S.ItemTitleInfo>
+                <S.ItemTitlePoint>적립금</S.ItemTitlePoint>
+                <S.ItemTitlePost>배송구분</S.ItemTitlePost>
+                <S.ItemTitlePostPrice>배송비</S.ItemTitlePostPrice>
+                <S.ItemTitleTotalPrice>합계</S.ItemTitleTotalPrice>
+                <S.ItemTitleSelect>선택</S.ItemTitleSelect>
+            </S.ItemTitleBox>
+            {
+                <S.ItemBox>
+                    <input type='checkbox' />
+                    <div>
+                        <img src='' alt='' />
+                        <div>
+                            <div></div>
+                            <div>옵션변경</div>
+                        </div>
+                    </div>
+                    <div>-</div>
+                    <div>-</div>
+                    <div>-</div>
+                    <div>39900</div>
+                    <div>
+                        <button>주문하기</button>
+                        <button>장바구니</button>
+                        <button>삭제</button>
+                    </div>
+                </S.ItemBox>
+            }
         </S.Contain>
     )
 }
--- src/components/units/mypage/mypageWishList/WisthList.style.tsx
+++ src/components/units/mypage/mypageWishList/WisthList.style.tsx
@@ -11,5 +11,65 @@ export const PageTitle = styled.div`
     font-weight: 900;
     padding-bottom: 12px;
     border-bottom: 2px solid black;
-    margin-bottom: 30px;
+`
+
+export const ItemTitleBox = styled.div`
+    display: flex;
+    padding: 10px;
+    justify-content: center;
+    align-items: center;
+    border-bottom: 1px solid lightgray;
+`
+
+export const ItemBox = styled.div`
+    display: flex;
+    padding: 10px;
+    justify-content: center;
+    align-items: center;
+    border-bottom: 1px solid lightgray;
+`
+
+export const ItemTitleCheckBox = styled.input`
+    width: 10%;
+    display: flex;
+    justify-content: center;
+    border: 1px solid lightgray;
+`
+
+export const ItemTitleInfo = styled.div`
+    width: 40%;
+    display: flex;
+    justify-content: center;
+    font-size: 13px;
+`
+
+export const ItemTitlePoint = styled.div`
+    width: 10%;
+    display: flex;
+    justify-content: center;
+    font-size: 13px;
+`
+export const ItemTitlePost = styled.div`
+    width: 10%;
+    display: flex;
+    justify-content: center;
+    font-size: 13px;
+`
+export const ItemTitlePostPrice = styled.div`
+    width: 10%;
+    display: flex;
+    justify-content: center;
+    font-size: 13px;
+`
+export const ItemTitleTotalPrice = styled.div`
+    width: 10%;
+    display: flex;
+    justify-content: center;
+    font-size: 13px;
+`
+export const ItemTitleSelect = styled.div`
+    width: 10%;
+    display: flex;
+    justify-content: center;
+    font-size: 13px;
 `
