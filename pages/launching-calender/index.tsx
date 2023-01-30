754ed1cceae374f663410e873ff29d3dc05fd2c5
JeongHyeonSeok <hyeonseok45@gmail.com>
2023-01-31 03:22:39 +0900

    launching-calender complete

---
+++ pages/launching-calender/index.tsx
@@ -0,0 +1,10 @@
+import LaunchingCalenderList from '../../src/components/units/launching-calender/launching-calender-list/LaunchingCalenderList'
+
+
+const LaunchingCalenderPage = () => {
+    return (
+        <LaunchingCalenderList />
+    )
+}
+
+export default LaunchingCalenderPage
