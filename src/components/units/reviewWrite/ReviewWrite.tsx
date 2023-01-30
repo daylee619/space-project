9a248554920827d6906e4c262045035786f3151c
JeongHyeonSeok <hyeonseok45@gmail.com>
2023-01-31 03:31:27 +0900

    save

--- src/components/units/reviewWrite/ReviewWrite.tsx
+++ src/components/units/reviewWrite/ReviewWrite.tsx
@@ -1,33 +1,18 @@
-import axios from 'axios'
-import { useRef, useState } from 'react'
 
 
 const ReviewWrite = () => {
-    const imgRef = useRef<any>(null)
-    const [postFile, setPostFile] = useState<any>([])
-
-    const changeHandler = () => {
-        setPostFile((prev: any) => [...prev, imgRef.current?.files[0]])
-        // setPostFile(imgRef.current?.files[0])
-        // console.log('1', postFile)
-    }
-
-    const postHandler = async () => {
-        try {
-            console.log('2', postFile)
-            await axios.post('http://192.168.232.162:3000/review/product', {
-                files: postFile
-            }, {
-                headers: { "Content-Type": "multipart/form-data" }
-            })
-        } catch (error) {
-            console.log(error)
-        }
-    }
     return (
         <div>
-            <input type='file' ref={imgRef} onChange={changeHandler} accept=".gif, .jpg, .png" />
-            <button onClick={postHandler}>post</button>
+            <div>
+
+            </div>
+            <div>
+                <div>
+                    <input type='text' />
+                    <button>등록</button>
+                </div>
+                <input type='file' />
+            </div>
         </div>
     )
 }
