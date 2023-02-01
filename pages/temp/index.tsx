14896f6d30eb9488477c1a685223c2fc735d8d01
JeongHyeonSeok <hyeonseok45@gmail.com>
2023-02-01 22:44:06 +0900

    complete

--- pages/temp/index.tsx
+++
@@ -1,9 +0,0 @@
-import PlusOptionModal from '../../src/common/utils/optionModal/wishListOptionModal/PlusOptionModal'
-
-const Test = () => {
-    return (
-        <PlusOptionModal />
-    )
-}
-
-export default Test
