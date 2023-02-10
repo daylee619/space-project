609a0c5265aa7f081bbe2449dc196b46fb7e5336
daylee619 <110576332+daylee619@users.noreply.github.com>
2023-02-10 15:22:38 +0900

    save

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,26 @@
 import Head from "next/head"
 import Portal from "../src/components/Portal.js"
+import CarouselFadeExample from "../src/components/units/main/mainCarousel"
+import "bootstrap/dist/css/bootstrap.min.css"
+import MainWeeklyBest from "../src/components/units/main/mainWeeklyBest"
+import MainNew from "../src/components/units/main/mainNew"
+import MainPhotoReview from "../src/components/units/main/mainPhotoReview"
+import MainLookBook from "../src/components/units/main/mainLookBook"
+import MainSnap from "../src/components/units/main/mainSnap"
+import MainLookBookDetailCarousel from "../src/components/units/main/mainLookBook/mainLookBookCarousel/mainLookBookDetailCarousel"
 export default function Home() {
   return (
     <>
       <Head></Head>
+      <>
+        <CarouselFadeExample />
+        <MainWeeklyBest />
+        <MainNew />
+        <MainPhotoReview />
+        <MainLookBook />
+        <MainSnap />
+        <MainLookBookDetailCarousel />
+      </>
       <Portal>{/* <MyComponent /> */}</Portal>
     </>
   )
